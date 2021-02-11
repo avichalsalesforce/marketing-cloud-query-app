8a6059d801b0bfc970dab8a509f4df4d00a9ee09
Avichal Mishra <mishraavi650@gmail.com>
2021-02-11 12:05:10 +0530

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const app = express()
 const path = require('path');
+const router = express.Router();
 
 // use the express-static middleware
 app.use(express.static("marketing-cloud-query-app"))
@@ -10,9 +11,9 @@ app.use(express.static("marketing-cloud-query-app"))
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + '/loginpage.html'));
 })
-app.get('/myButton', function(req, res){ 
-    res.sendFile(path.join(__dirname + '/secondpage.html')); 
-});
+router.get('/secondpage',function(req,res){
+    res.sendFile(path.join(__dirname+'/secondpage.html'));
+  });
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
