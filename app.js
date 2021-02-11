afd8c44680f83a64d631049a14232ed3e5b533fd
Avichal Mishra <mishraavi650@gmail.com>
2021-02-11 11:22:05 +0530

    commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + '/loginpage.html'));
 })
 app.get('/loginpage.html', function(req, res){ 
-    res.send(path.join(__dirname + '/secondpage.html')); 
+    res.send(path.join('/secondpage.html')); 
 });
 
 // start the server listening for requests
