c1af6fcb058e56224007fd2b7d958790365a71b8
Avichal Mishra <mishraavi650@gmail.com>
2021-02-11 19:08:57 +0530

    commit

--- app.js
+++ app.js
@@ -22,31 +22,40 @@ app.post("/secondpage", function (req, res) {
    var clinentauthurl= req.body.authurl;
    console.log('Avi'+ clientidSource,'Avi1'+ clientsecretSource,'Avi2'+ clinentauthurl);
    //alert('Avi'+ clientidSource,'Avi1'+ clientsecretSource,'Avi2'+ clinentauthurl);
-  // res.sendFile(path.join(__dirname + '/secondpage.html'));
+
+
+
+   var payload = '{"grant_type":"client_credentials",';
+   payload += '"client_id":"jye6cem725bblk5cghdzes5g",';
+   payload += '"client_secret":"vC2k5frAF8vdiyexkCJPCb4Q",';
+   payload += '"scope":null,';
+   payload += '"account_id":"514011820"}';
+var url = "https://mc6vgk-sxj9p08pqwxqz9hw9-4my.rest.marketingcloudapis.com/v2/token";
+var contentType = 'application/json';
+try {
+    /* perform HTTP Post request to get a new OAuth 2.0 access token */
+    var accessTokenResult = HTTP.Post(url, contentType, payload);
+    var tokenObj = Platform.Function.ParseJSON(accessTokenResult["Response"][0]);
+    var accessToken = tokenObj.access_token;
+    console.log("OAuth 2.0 Access Token: " + accessToken);
+   // Write("OAuth 2.0 Access Token: " + accessToken);
+} catch(e) {
+   //Write(Stringify(e));
+   console.log(e);
+}
+
+
+
+
+
+   res.sendFile(path.join(__dirname + '/secondpage.html'));
 
 });
 
 
 // Platform.Load("Core", "1");
 /* create the payload in JSON format */
-var payload = '{"grant_type":"client_credentials",';
-      payload += '"client_id":"jye6cem725bblk5cghdzes5g",';
-      payload += '"client_secret":"vC2k5frAF8vdiyexkCJPCb4Q",';
-      payload += '"scope":null,';
-      payload += '"account_id":"514011820"}';
-var url = "https://mc6vgk-sxj9p08pqwxqz9hw9-4my.rest.marketingcloudapis.com/v2/token";
-var contentType = 'application/json';
-try {
-       /* perform HTTP Post request to get a new OAuth 2.0 access token */
-       var accessTokenResult = HTTP.Post(url, contentType, payload);
-       var tokenObj = Platform.Function.ParseJSON(accessTokenResult["Response"][0]);
-       var accessToken = tokenObj.access_token;
-       console.log("OAuth 2.0 Access Token: " + accessToken);
-      // Write("OAuth 2.0 Access Token: " + accessToken);
-} catch(e) {
-      //Write(Stringify(e));
-      console.log(e);
- }
+
    
 
 
