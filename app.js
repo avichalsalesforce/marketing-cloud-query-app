2655f0576a3e87d6ae6b6561580d9d3c14077565
Avichal Mishra <mishraavi650@gmail.com>
2021-02-11 12:28:37 +0530

    commit

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ app.use(express.static("marketing-cloud-query-app"))
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + '/loginpage.html'));
 })
-app.get('/secondpage',function(req,res){
-    res.sendFile(path.join(__dirname + '/secondpage.html'));
-  });
+app.get('/secondpage', (req, res) => {
+    res.sendFile('secondpage.html', {
+        root: path.join(__dirname, './')
+    })
+})
+
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
