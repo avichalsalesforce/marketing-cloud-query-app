03bbf4219c1e5772b010f4cbbc265e3c71ec1a6e
Avichal Mishra <mishraavi650@gmail.com>
2021-02-15 17:24:28 +0530

    commit

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post("/secondpage", function (req, res) {
 
    var data1 = '<?xml version="1.0" encoding="UTF-8"?>\r\n<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">\r\n    <s:Header>\r\n        <a:Action s:mustUnderstand="1">Retrieve</a:Action>\r\n        <a:To s:mustUnderstand="1">https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx</a:To>\r\n        <fueloauth xmlns="http://exacttarget.com">token</fueloauth>\r\n    </s:Header>\r\n    <s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">\r\n        <RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">\r\n            <RetrieveRequest>\r\n                <ObjectType>DataExtension</ObjectType>\r\n                <Properties>ObjectID</Properties>\r\n                <Properties>CustomerKey</Properties>\r\n                <Properties>Name</Properties>\r\n                <Properties>IsSendable</Properties>\r\n                <Properties>SendableSubscriberField.Name</Properties>\r\n               \r\n            </RetrieveRequest>\r\n        </RetrieveRequestMsg>\r\n    </s:Body>\r\n</s:Envelope>';
   
-  var config = {
+  var config1 = {
     method: 'post',
     url: 'https://mc6vgk-sxj9p08pqwxqz9hw9-4my.soap.marketingcloudapis.com/Service.asmx',
     headers: { 
@@ -65,9 +65,9 @@ app.post("/secondpage", function (req, res) {
     data : data1
   };
   
-  axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
+  axios(config1)
+  .then(function (response1) {
+    console.log(JSON.stringify(response1.data));
   })
   .catch(function (error) {
     console.log(error);
